47fa503e431b7f683b1f215931f04728a7dae0ce
Gaurav Mishra <gauravmishra2401@gmail.com>
2024-01-05 19:53:06 +0530

    added utility files

---
+++ src/utils/ApiError.util.js
@@ -0,0 +1,21 @@
+class ApiError extends Error {
+    constructor(
+        statusCode,
+        message = "Something went wrong",
+        errors = [],
+        stack = "",
+    ) {
+        super(message);
+        this.statusCode = statusCode;
+        this.message = message;
+        this.success = false;
+        this.data = null;
+        this.errors = this.errors;
+
+        if (stack) {
+            this.stack = stack;
+        } else {
+            Error.saptureStackTrace(this, this.constructor);
+        }
+    }
+}
