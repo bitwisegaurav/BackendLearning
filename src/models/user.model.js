b711147edd948bea96172b192ce8617c25cde79d
Gaurav Mishra <gauravmishra2401@gmail.com>
2024-01-06 15:13:34 +0530

    added user schema

---
+++ src/models/user.model.js
@@ -0,0 +1,51 @@
+import mongoose, { Schema } from "mongoose";
+
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            required: true,
+            unique: true,
+            trim: true,
+            lowercase: true,
+            minlength: 3,
+            maxlength: 20,
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            trim: true,
+            lowercase: true,
+        },
+        password: {
+            type: String,
+            required: true,
+            minlength: 6,
+            maxlength: 20,
+        },
+        fullName: {
+            type: String,
+            required: true,
+            trim: true,
+            lowercase: true,
+        },
+        avatar: {
+            type: String, // link
+            required: true,
+        },
+        coverImage: {
+            type: String, // link
+        },
+        refreshToken: {
+            type: String,
+        },
+        watchHistory: {
+            type: Schema.Types.ObjectId,
+            ref: "Video",
+        },
+    },
+    { timestamps: true },
+);
+
+export const User = mongoose.model("User", userSchema);
