2b772cfeda0e179d23306bed5411a1a69383fffb
Gaurav Mishra <gauravmishra2401@gmail.com>
2024-01-06 18:41:22 +0530

    added code for file upload on cloudinary

---
+++ src/utils/cloudinary.util.js
@@ -0,0 +1,30 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+import ApiError from "./ApiError.util";
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const uploadImage = async (localFilePath) => {
+    if (!localFilePath) return null;
+    try {
+        const cloudinaryUploadValue = await cloudinary.uploader.upload(
+            localFilePath,
+            { resource_type: "auto" },
+        );
+        console.log(`File uplaoded. ${cloudinaryUploadValue.url}`);
+        console.log(cloudinaryUploadValue);
+        fs.unlinkSync(localFilePath);
+        return cloudinaryUploadValue;
+    } catch (error) {
+        fs.unlinkSync(localFilePath);
+        console.log(error);
+        // return new ApiError(500, "File is not uploaded on cloudinary");
+        return null;
+    }
+};
+
+export { uploadImage };
