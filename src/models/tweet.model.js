2bb6e0a0d2efd2a558a30d57fd1b4496d6b47935
Gaurav Mishra <gauravmishra2401@gmail.com>
2024-01-06 15:26:33 +0530

    added tweet model

---
+++ src/models/tweet.model.js
@@ -0,0 +1,20 @@
+import mongoose, { Schema } from "mongoose";
+
+const tweetSchema = new Schema(
+    {
+        owner: {
+            type: Schema.Types.ObjectId,
+            ref: "User"
+        },
+        content: {
+            type: String,
+            required: true,
+            trim: true,
+            minlength: 1,
+            maxlength: 500,
+        },
+    },
+    { timestamps: true },
+);
+
+export const Tweet = mongoose.model("Tweet", tweetSchema);
