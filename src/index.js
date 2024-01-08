ffd904b249824b6a0127bbe2fc41a1dcb40fe8ba
Gaurav Mishra <gauravmishra2401@gmail.com>
2024-01-08 14:23:52 +0530

    formatted the index file

--- src/index.js
+++ src/index.js
@@ -3,16 +3,14 @@ import dotenv from "dotenv";
 import { app } from "./app.js";
 import { connectDB } from "./database/connection.db.js";
 
-dotenv.config({path: './.env'})
-
+dotenv.config({ path: "./.env" });
 
 connectDB()
-.then(() => {
-    app.listen(process.env.PORT || 3000, () => {
-        console.log("Server is running at", (process.env.PORT || 3000));
+    .then(() => {
+        app.listen(process.env.PORT || 3000, () => {
+            console.log("Server is running at", process.env.PORT || 3000);
+        });
+    })
+    .catch((err) => {
+        console.log("Database connection failed. Error : ", err);
     });
-})
-.catch(err => {
-    console.log("Database connection failed. Error : ", err);
-})
-;
