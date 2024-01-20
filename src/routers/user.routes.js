092f0286473ead7c8c24b7237ccd307858b017c6
Gaurav Mishra <gauravmishra2401@gmail.com>
2024-01-21 02:45:49 +0530

    Add user routes for updating password, user details, avatar, and cover image

--- src/routers/user.routes.js
+++ src/routers/user.routes.js
@@ -1,9 +1,14 @@
 import { Router } from "express";
 import {
+    getCurrentUser,
     loginUser,
     logoutUser,
     refreshAccessToken,
     registerUser,
+    updateUserAvatarImage,
+    updateUserCoverImage,
+    updateUserDetails,
+    updateUserPassword,
 } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
@@ -23,5 +28,14 @@ router.route("/login").post(loginUser);
 // secured routes
 router.route("/logout").post(verifyJWT, logoutUser);
 router.route("/refresh-token").post(refreshAccessToken);
+router.route("/update-password").post(verifyJWT, updateUserPassword);
+router.route("/update-user-details").post(verifyJWT, updateUserDetails);
+router.route("/get-user-details").get(verifyJWT, getCurrentUser);
+router
+    .route("/update-avatar")
+    .post(verifyJWT, upload.single("avatar"), updateUserAvatarImage);
+router
+    .route("/update-cover-image")
+    .post(verifyJWT, upload.single("coverImage"), updateUserCoverImage);
 
 export default router;
